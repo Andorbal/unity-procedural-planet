af79115a3b24fe1dbd5c3ad7a3048e56ad96e17b
Andrew Benz <andorbal@gmail.com>
2023-02-08 22:00:52 -0600

    Add multiple layers of noise

--- Assets/NoiseFilter.cs
+++ Assets/NoiseFilter.cs
@@ -14,7 +14,19 @@ public class NoiseFilter
 
   public float Evaluate(Vector3 point)
   {
-    float noiseValue = (noise.Evaluate(point * settings.roughness + settings.center) + 1) * .5f;
+    float noiseValue = 0;
+    float frequency = settings.baseRoughness;
+    float amplitude = 1;
+
+    for (var i = 0; i < settings.numLayers; i += 1)
+    {
+      float v = noise.Evaluate(point * frequency + settings.center);
+      noiseValue += (v + 1) * .5f * amplitude;
+      frequency *= settings.roughness;
+      amplitude *= settings.persistence;
+    }
+
+    noiseValue = Mathf.Max(0, noiseValue - settings.minValue);
     return noiseValue * settings.strength;
   }
 }
