456269702682c830afbc8096df0e5dccb4dcad5a
Andrew Benz <andorbal@gmail.com>
2023-02-15 18:52:00 -0600

    Add ocean specific gradient

--- Assets/ShapeGenerator.cs
+++ Assets/ShapeGenerator.cs
@@ -20,7 +20,7 @@ public class ShapeGenerator
     }
   }
 
-  public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
+  public float CalculateUnscaledElevation(Vector3 pointOnUnitSphere)
   {
     float firstLayerValue = 0;
     float elevation = 0;
@@ -43,8 +43,14 @@ public class ShapeGenerator
       }
     }
 
-    elevation = settings.planetRadius * (1 + elevation);
     this.elevationMinMax.AddValue(elevation);
-    return pointOnUnitSphere * elevation;
+    return elevation;
+  }
+
+  public float GetScaledElevation(float unscaledElevation)
+  {
+    float elevation = Mathf.Max(0, unscaledElevation);
+    elevation = settings.planetRadius * (1 + elevation);
+    return elevation;
   }
 }
